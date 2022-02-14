2a4fbc2172afc8d516d3215bbee598295dec87ad
Higor Martinez <73290985+higormartinezo@users.noreply.github.com>
2022-02-14 12:24:57 -0300

    Update Simple Climate Model 2020.R

--- Simple Climate Model 2020.R
+++ Simple Climate Model 2020.R
@@ -1,4 +1,4 @@
-#SIMPLE CLIMATE MODEL - ENERGY BUDGET
+#SIMPLE CLIMATE MODEL - ENERGY BUDGET 
 
 #PART 1 - THE BLACK BODY MODEL 
 
@@ -19,8 +19,8 @@ incoming_solar_energy =
 blackbody_temperature =
   (((incoming_solar_energy)/(4*(0.0000000567)))^0.25) 
 
-#PART 2 - SURFACE FEATURES 
-#The area is given in Percentage. 
+#PART 2 - SURFACE FEATURES AND CLOUDS
+#Area is given in percentage. 
 
 #Surface composition: Ocean 
 ocean_surface_area = 0.7
@@ -38,28 +38,21 @@ desert_albedo = 0.2
 ice_surface_area = 0.05 
 ice_albedo = 0.7 
 
+#Clouds
+cloud_area = 0.7
+cloud_albedo = 0.3
+
 #Solar Energy Reflected 
 ocean_energy_reflected = ((incoming_solar_energy)*(ocean_surface_area)*(ocean_albedo))
 vegetation_energy_reflected = ((incoming_solar_energy)*(vegetation_surface_area)*(vegetation_albedo))
 desert_energy_reflected = ((incoming_solar_energy)*(desert_surface_area)*(desert_albedo))
 ice_energy_reflected = ((incoming_solar_energy)*(ice_surface_area)*(ice_albedo))
+cloud_energy_reflected = ((incoming_solar_energy)*(cloud_area)*(cloud_albedo))
 
-surface_energy_reflected = (ocean_energy_reflected + vegetation_energy_reflected + desert_energy_reflected + ice_energy_reflected)
+surface_energy_reflected = (ocean_energy_reflected + vegetation_energy_reflected + desert_energy_reflected + ice_energy_reflected + cloud_energy_reflected)
 
 #Solar Energy Absorbed 
-energy_absorbed = (incoming_solar_energy - surface_energy_reflected)
-
-#Temperature considering reflection at surface (Kelvin)
-temperature_part2 = 
-  (((energy_absorbed)/(4*(0.0000000567)))^0.25)
-
-#PART3 - CLOUDS
-cloud_area = 0.7
-cloud_albedo = 0.3
-
-cloud_energy_reflected = ((incoming_solar_energy)*(cloud_area)*(cloud_albedo)) 
-
-total_energy_absorbed = (incoming_solar_energy)-(surface_energy_reflected+cloud_energy_reflected)
+total_energy_absorbed = (incoming_solar_energy - surface_energy_reflected)
 
 #Planetary Albedo 
 planetary_albedo = 
@@ -69,9 +62,9 @@ planetary_albedo =
 effective_temperature = 
   ((incoming_solar_energy/4*(1-planetary_albedo)/0.0000000567)^0.25)
 
-#PART 4 - GREENHOUSE EFFECT 
+#PART 3 - GREENHOUSE EFFECT 
 
-#Concentrations (ppm)
+#Concentrations (given in ppm)
 CO2_concentration = 411
 CH4_concentration = 1.87
 
@@ -89,7 +82,7 @@ total_T = TCO2+TCH4+TH20
 #Emissivity 
 emissivity = 1/(1+0.75*total_T)
 
-#PART 5 - SURFACE TEMPERATURE 
+#PART 4 - SURFACE TEMPERATURE 
 
 #Stefan–Boltzmann law
 E = (0.0000000567*effective_temperature^4) 
